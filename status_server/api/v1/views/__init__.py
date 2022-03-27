e6e4fb93a0f373152c85fc79dafe181c176ada8e
dnjoe96 <donjoedbest@gmail.com>
2022-03-27 16:58:24 +0100

    [Fix] Fixed the code
    The status was initally returning an error 404 for the route
    curl -XGET http://0.0.0.0:5000/api/v1/status
    
    This was because the root route was set to /api/v1. so the correct route
    would have been curl -XGET http://0.0.0.0:5000/api/vi/api/v1/status
    which is not the right route. To fix the issue, I changed it to "/"

--- status_server/api/v1/views/__init__.py
+++ status_server/api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """ Views module
 """
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# to fix the code, i changed url_prefix from /api/v1 to / because 
+# this indicates the root of the routes
+app_views = Blueprint("app_views", __name__, url_prefix="/")
 
 from api.v1.views.index import *
